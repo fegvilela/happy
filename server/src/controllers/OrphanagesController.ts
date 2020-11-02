a20cd9ad62796e20b3390c6300df0bc17cb6670e
Fernanda Vilela <fegvilela@gmail.com>
2020-11-02 11:32:49 -0300

    add image controler e multer uploader

---
+++ server/src/controllers/OrphanagesController.ts
@@ -0,0 +1,58 @@
+import { Request, Response } from "express";
+import { getRepository } from "typeorm";
+import Orphanage from "../models/Orphanage";
+
+export default {
+  async index(request: Request, response: Response) {
+    const orphanagesRepository = getRepository(Orphanage);
+
+    const orphanages = await orphanagesRepository.find();
+
+    return response.json(orphanages);
+  },
+  async show(request: Request, response: Response) {
+    const { id } = request.params;
+
+    const orphanagesRepository = getRepository(Orphanage);
+
+    const orphanage = await orphanagesRepository.findOneOrFail(id);
+
+    return response.json(orphanage);
+  },
+
+  async create(request: Request, response: Response) {
+    const {
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends,
+    } = request.body;
+
+    const orphanagesRepository = getRepository(Orphanage);
+    const requestImages = request.files as Express.Multer.File[];
+
+    const images = requestImages.map(image => {
+      return {
+        path: image.filename,
+      }
+    });
+
+    const data = {
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends,
+      images,
+    };
+
+    await orphanagesRepository.save(data);
+
+    return response.status(201).json(data);
+  },
+};
